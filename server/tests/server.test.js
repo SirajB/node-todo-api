0ba085df518f5642c00a01feb07a5020b78a9177
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-07-31 14:03:06 +0100

    Added route GET /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,17 @@ const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test dodo'
+}];
+
 //Runs before describe below, wipes database so that we our test is correct
 beforeEach((done) => {
-    Todo.remove({}).then(() => done()) //.then(() => { done() }
+    Todo.remove({}).then(() =>{
+        Todo.insertMany(todos);
+    }).then(() => done()) // .then(() => { done() })
 });
 
 
@@ -27,7 +35,7 @@ describe('Post /todos', () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -48,9 +56,21 @@ describe('Post /todos', () => {
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e))
         })
     })
+});
+
+describe ('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
 });
