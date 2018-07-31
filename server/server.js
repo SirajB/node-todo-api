f0c09837b4f0079ded8b855d81ae6bbb0eca1bd4
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-07-31 15:48:06 +0100

    Created test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -40,7 +40,9 @@ app.get('/todos/:id', (req,res) => {
             res.status(404).send({})
         }
         res.status(200).send({todo});
-    }).catch((e) => res.status(400).send())
+    }).catch((e) => {
+        res.status(400).send()
+    });
 });
 
 
