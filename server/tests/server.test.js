8456a0c83e09144725cb095ab5c6a7be3ed5b12b
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-08-01 09:45:45 +0100

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -143,3 +143,45 @@ describe('DELETE /todos/:id', () => {
         .end(done)
     });
 });
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+        const text = 'This is a new test';
+        
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: true,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text),
+            expect(res.body.todo.completed).toBe(true),
+            expect(res.body.todo.completedAt).toBeA('number')
+            
+        })
+        .end(done)
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        let hexId = todos[1]._id.toHexString();
+        const text = 'This is a new test mrk 2';
+        
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: false,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text),
+            expect(res.body.todo.completed).toBe(false),
+            expect(res.body.todo.completedAt).toNotExist();
+            
+        })
+        .end(done)
+    })
+})
