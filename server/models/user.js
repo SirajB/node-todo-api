bda398f27cafbd56b956cf6394799bcc2466bd67
Siraj Brepotra <sirajbrepotra@gmail.com>
2018-08-02 16:13:28 +0100

    update config to use JSON file

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,4 @@
+require('./../config/config.js')
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken')
@@ -46,7 +47,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     let user = this;
     let access = 'auth';
-    let token = jwt.sign ({_id: user._id.toHexString(), access}, 'abc123').toString();
+    let token = jwt.sign ({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens = user.tokens.concat({access, token});
 
@@ -74,7 +75,7 @@ UserSchema.statics.findByToken = function (token) {
     let decoded;
   
     try {
-      decoded = jwt.verify(token, 'abc123');
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
       return Promise.reject();
     }
